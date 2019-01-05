05cb608ef14882ca4ed7a93dd02bfa896668d531
Fluffy1993 <michal.futera@seznam.cz>
2019-01-06 00:57:38 +0100

    second

---
+++ src/playground/es6-let-const.js
@@ -0,0 +1,22 @@
+var nameVar = 'Michal';
+var nameVar = 'Zavi';
+console.log('nameVar', nameVar);
+
+
+let nameLet = 'Jan';
+nameLet = 'Kokot';
+console.log('nameLet', nameLet);
+
+const nameConst = 'Frank';
+console.log('nameConst', nameConst);
+
+
+const fullName = 'Petr Zavadil';
+const firstName;
+
+if (fullName) {
+    firstName = fullName.split(' ')[0];
+    console.log(firstName);
+}
+
+console.log(firstName);
